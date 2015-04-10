f3097cab1aae09a66925677233032571e0381e36
bdabeast456 <brandonpearl@berkeley.edu>
2015-04-10 14:03:17 -0700

    Might compile

--- bezier.cpp
+++ bezier.cpp
@@ -177,7 +177,11 @@ void tessellate(Surface s, double step, double u, double v) {
   vector<double> point3 = s.getSurfacePoint(u+step, v+step);
   vector<double> point4 = s.getSurfacePoint(u, v+step);
   if (!tessellationStrat) {
-    vector<vector<double> > poly[4] = {point1, point2, point3, point4};
+    vector<vector<double> > poly;
+    poly.push_back(point1);
+    poly.push_back(point2);
+    poly.push_back(point3);
+    poly.push_back(point4);
     polygons.push_back(Polygon(poly, currID));
   } else {
     vector<double> actual = s.getSurfacePoint((4*u+2*step)/4, (4*v+2*step)/4);
@@ -186,7 +190,11 @@ void tessellate(Surface s, double step, double u, double v) {
       current.push_back((point1[j]+point2[j]+point3[j]+point4[j])/4);
     }
     if (distance(current, actual) < step) {
-      vector<vector<double> > poly[4] = {point1, point2, point3, point4};
+      vector<vector<double> > poly;
+      poly.push_back(point1);
+      poly.push_back(point2);
+      poly.push_back(point3);
+      poly.push_back(point4);
       polygons.push_back(Polygon(poly, currID));
     } else {
       double halfStep = step/2;

af156f6a2bca8edb3a13f48df1fe204bc795cd46
bdabeast456 <brandonpearl@berkeley.edu>
2015-04-13 00:47:56 -0700

    Sync
    Pow test.

--- bezier.cpp
+++ bezier.cpp
@@ -18,7 +18,6 @@
 #endif
 
 #include <time.h>
-#include <math.h>
 #include <iostream>
 #include <algorithm>
 #include <iostream>
@@ -28,6 +27,7 @@
 #include <cstdlib>
 #include <vector>
 #include <cmath>
+#include <math.h>
 
 #include "bezier_util.h"
 
@@ -129,7 +129,7 @@ bool distance(double x1, double y1, double z1, vector<double> coords) {
     /*
     * Tells if errorBound is larger than the distance between points.
     */
-    if (errorBound > sqr(sqr(x1-coords[0])+sqr(y1-coords[1])+sqr(z1-coords[2]))) { 
+    if (errorBound > pow(sqr(x1-coords[0])+sqr(y1-coords[1])+sqr(z1-coords[2]), 2)) { 
         return true;
     } else { 
         return false;
